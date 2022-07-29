09fd2d77fa9bc137e2c6212662d84cec9b61473b
DOGA TANSEL <bupsdogatansel@gmail.com>
2022-07-29 08:31:50 +0300

    Card changes

--- src/components/TaskBoard/index.jsx
+++ src/components/TaskBoard/index.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
-import { Typography, Box, Card, CardHeader, CardMedia, CardContent, CardActions, Button } from '@mui/material';
+import { Typography, Box, Card, CardHeader, CardMedia, CardContent, CardActions, Button, Divider } from '@mui/material';
+import { ListItem, ListItemText, ListItemButton, List  } from '@mui/material';
 
 
-export default function TaskBoard(tasks, setTasks, currentTask, setCurrentTask){
+export default function TaskBoard(tasks, setTasks, currentTask, setCurrentTask, selectedFolder){
     console.log(tasks);
 
     return (
@@ -13,21 +14,36 @@ export default function TaskBoard(tasks, setTasks, currentTask, setCurrentTask){
         >
             <Card sx={{ maxWidth: 345 }}>
                 <CardHeader
-                    title="Bişey Bişey Bişey"
-                    subheader="September 14, 2016"
+                    title="Task Name"
+                    subheader="Selected Folder (PUT ICON KARDEŞM)"
+                    
                 />   
-                
-                <CardMedia
-                    component="img"
-                    height="194"
-                    image="/static/images/cards/paella.jpg"
-                    alt="Paella dish"
-                />
+
+                <Divider />
+
                 <CardContent>
-                    <Typography variant="body2" color="text.secondary">
-                        This impressive paella is a perfect party dish and a fun meal to cook
-                        together with your guests. Add 1 cup of frozen peas along with the mussels,
-                        if you like.
+                    <Typography variant="h5" color="text.secondary">
+                        Assignee Name
+                    </Typography>
+
+                    <Divider />
+
+                    <List>
+                        <ListItem disablePadding>
+                            <ListItemText primary="some subtask item"/>
+                        </ListItem>
+                        <ListItem disablePadding>
+                            <ListItemText primary="another subtask item"/>
+                        </ListItem>
+                        <ListItem disablePadding>
+                            <ListItemText primary="last subtask item"/>
+                        </ListItem>
+                    </List>
+
+                    <Divider />
+
+                    <Typography variant="h5" color="text.secondary">
+                        Due: Deadline
                     </Typography>
                 </CardContent>
             </Card>
