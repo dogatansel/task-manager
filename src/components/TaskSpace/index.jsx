1178355a939b40bd1e4cd5b49f3ab30ba19a8270
DOGA TANSEL <bupsdogatansel@gmail.com>
2022-08-08 12:04:35 +0300

    taskspace

--- src/components/TaskSpace/index.jsx
+++ src/components/TaskSpace/index.jsx
@@ -5,10 +5,11 @@ import { Box } from '@mui/material';
 
 export default function TaskSpace({drawerWidth}) {
     return (
-        <Box sx={{ display: 'flex' }}>
-            
-            <TaskBoard/>
+        <Box sx={{ display: 'block' }}>
+
             <Navbar drawerWidth={drawerWidth}/>
+            <TaskBoard/>
+
         </Box>
     );
 }
